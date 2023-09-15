da93a07ac4881c3d9415772d7ee95cb589f49b56
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-09-15 14:07:24 +0200

    python3Packages.xmlschema: 2.3.1 -> 2.4.0

--- pkgs/development/python-modules/xmlschema/default.nix
+++ pkgs/development/python-modules/xmlschema/default.nix
@@ -10,7 +10,7 @@
 
 buildPythonPackage rec {
   pname = "xmlschema";
-  version = "2.3.1";
+  version = "2.4.0";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
@@ -19,7 +19,7 @@ buildPythonPackage rec {
     owner = "sissaschool";
     repo = "xmlschema";
     rev = "refs/tags/v${version}";
-    hash = "sha256-0xXA3IguVAyFp5dFvuzAQhzJlGMmNthmPXcja9FYV44=";
+    hash = "sha256-oUCqKDauBxmWsFfxj4LcmPJMkz352iO8PZbrDUtFrP8=";
   };
 
   propagatedBuildInputs = [
@@ -30,6 +30,11 @@ buildPythonPackage rec {
     jinja2
     lxml
     pytestCheckHook
+  ];
+
+  disabledTests = [
+    # https://github.com/sissaschool/xmlschema/issues/359
+    "test_pl_validation_translation"
   ];
 
   pythonImportsCheck = [
