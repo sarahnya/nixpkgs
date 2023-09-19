9a1db3173630629b1f32c170025e61ea124b0a24
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-09-19 16:27:20 +0200

    fixup! python3Packages.sphinxcontrib-confluencebuilder: 2.0.0 -> 2.3.0

--- pkgs/development/python-modules/sphinxcontrib-confluencebuilder/default.nix
+++ pkgs/development/python-modules/sphinxcontrib-confluencebuilder/default.nix
@@ -1,6 +1,7 @@
 { lib
 , buildPythonPackage
 , fetchPypi
+, flit-core
 , docutils
 , sphinx
 , requests
@@ -10,11 +11,17 @@
 buildPythonPackage rec {
   pname = "sphinxcontrib-confluencebuilder";
   version = "2.3.0";
+  format = "pyproject";
 
   src = fetchPypi {
-    inherit pname version;
+    pname = "sphinxcontrib_confluencebuilder";
+    inherit version;
     hash = "sha256-Zpe2n131afnq4IhQEEZtquvSEkkjv/uznXt2tQhGfZA=";
   };
+
+  nativeBuildInputs = [
+    flit-core
+  ];
 
   propagatedBuildInputs = [
     docutils
