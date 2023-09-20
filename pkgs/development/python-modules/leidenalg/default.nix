8d8a4bd0a4b24af4b8c2d49c5bc5140f7689a7c2
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-09-20 16:26:38 +0200

    Revert "python3Packages.leidenalg: 0.9.1 -> 0.10.1"
    This reverts commit a7457e98274a517c9f8c339c838e4a29a6e02fb8.
    
    Requires libleidenalg to be packaged separately, which I tried, but it
    didn't immediately work, so we leave this to the maintainer.

--- pkgs/development/python-modules/leidenalg/default.nix
+++ pkgs/development/python-modules/leidenalg/default.nix
@@ -11,14 +11,14 @@
 
 buildPythonPackage rec {
   pname = "leidenalg";
-  version = "0.10.1";
+  version = "0.9.1";
   format = "setuptools";
 
   disabled = pythonOlder "3.7";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-RXrZaYKoC9XGVxifQt/rd+6807dEoRDlosFhjS64C0c=";
+    hash = "sha256-flz+O2+A8yuQ9V81xo1KmQsEibEoLPP6usjNpJiJdfM=";
   };
 
   postPatch = ''
