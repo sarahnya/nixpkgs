50c704ff01a390922696fbb4119811adf4991b4a
Martin Weinelt <hexa@darmstadt.ccc.de>
2023-09-20 19:01:42 +0200

    fixup! python3Packages.librosa: 0.10.0 -> 0.10.1

--- pkgs/development/python-modules/librosa/default.nix
+++ pkgs/development/python-modules/librosa/default.nix
@@ -44,15 +44,6 @@ buildPythonPackage rec {
     hash = "sha256-zbmU87hI9A1CVcBZ/5FU8z0t6SS4jfJk9bj9kLe/EHI=";
   };
 
-  patches = [
-    # https://github.com/librosa/librosa/pull/1731
-    (fetchpatch {
-      name = "support-scipy-1.11.patch";
-      url = "https://github.com/librosa/librosa/commit/12dee8eabed7df14c5622b52c05393ddfeb11f4b.patch";
-      hash = "sha256-JxTXU0Mc+QYpsafjoGLaIccD7EdCYJvIVianeosYpw4=";
-    })
-  ];
-
   nativeBuildInputs = [
     setuptools
   ];
@@ -105,6 +96,19 @@ buildPythonPackage rec {
     "test_example"
     "test_example_info"
     "test_load_resample"
+    # https://github.com/librosa/librosa/issues/1754
+    "test_waveshow_mono"
+    "test_waveshow_mono_trans"
+    "test_waveshow_mono_zoom"
+    "test_waveshow_mono_zoom_trans"
+    "test_waveshow_mono_zoom_out"
+    "test_waveshow_stereo"
+    "test_unknown_wavaxis"
+    "test_waveshow_unknown_wavaxis"
+    "test_sharex_specshow_ms"
+    "test_sharex_waveplot_ms"
+    "test_waveshow_disconnect"
+    "test_waveshow_deladaptor"
   ];
 
   meta = with lib; {
